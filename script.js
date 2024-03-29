d082fb0b687fc973113ae017b1b6697f2bbd4ac9
Al Hannah <111653900+alHannah@users.noreply.github.com>
2024-03-29 19:20:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function toggleNav() {
     var navLinks = document.querySelector('.nav-links'); // Added dot before 'nav-links'
     navLinks.classList.toggle('show');
 }
+
+function dropDown() {
+    var dropdownLinks = document.querySelector('.dropdown-links'); // Added dot before 'nav-links'
+    dropdownLinks.classList.toggle('show');
+}
